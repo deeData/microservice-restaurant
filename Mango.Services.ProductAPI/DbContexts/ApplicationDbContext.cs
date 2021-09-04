458ff8a09bbccfd601121a22dcd0316b6929a156
deidra108@gmail.com <deidra108@gmail.com>
2021-09-04 10:43:06 -0600

    config db connection, create db and table via migration

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //create "Products" table in db via code migration
+        public DbSet<Product> Products { get; set; }
+    }
+}
