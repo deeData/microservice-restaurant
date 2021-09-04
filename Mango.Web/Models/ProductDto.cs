d6a9d3291ef6bd83db7f7e17053dbfc3bbf71979
deedata <deidra108@gmail.com>
2021-09-04 14:44:58 -0600

    implement general api request services in frontend

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mango.Services.ProductAPI.Models.Dto
+namespace Mango.Web.Models
 {
     public class ProductDto
     {
--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mango.Web.Models;
 
 namespace Mango.Web.Services.IServices
 {
@@ -10,9 +11,9 @@ namespace Mango.Web.Services.IServices
         //generic type T
         Task<T> GetAllProductsAsync<T>();
         Task<T> GetProductByIdAsync<T>(int id);
-        Task<T> CreateProductAsync<T>();
-        Task<T> UpdateProductAsync<T>();
-        Task<T> DeleteProductAsync<T>();
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
 
     }
 }
