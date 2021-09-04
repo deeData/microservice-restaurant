72ae692e6cf6bcecdae03b9616d7a6cbcf0d13bb
deidra108@gmail.com <deidra108@gmail.com>
2021-09-04 11:07:44 -0600

    setup AutoMapper for dto-model conversions

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps() 
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+                {
+                    //ReverseMap to also make available mapping from Product to ProductDto 
+                    config.CreateMap<ProductDto, Product>().ReverseMap();
+                });
+            return mappingConfig;
+        }
+    }
+}
