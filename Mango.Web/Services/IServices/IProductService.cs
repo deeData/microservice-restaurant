8f1a942e9035b7e047a90cf5b5803e090614768c
deedata <deidra108@gmail.com>
2021-09-04 14:03:35 -0600

    configure domain in frontend and setup Product dto/service in forntend

---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        //generic type T
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>();
+        Task<T> UpdateProductAsync<T>();
+        Task<T> DeleteProductAsync<T>();
+
+    }
+}
