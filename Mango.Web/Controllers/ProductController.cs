0b9a83ed840f9456c9420efcb524fd4da52833d6
deedata <deidra108@gmail.com>
2021-09-07 11:39:14 -0600

    create ProductController to frontend, pull list of products to product index page

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        //retrieve Product Service via dependency injection
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //ProductIndex in asp-action method from _layout html file
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+            //in ProductAPIController, will return ResponseDto 
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response!=null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
