5d0fdbf956a2430f7deed8a8440afdc92afe390e
deedata <deidra108@gmail.com>
2021-09-08 13:14:54 -0600

    setup ProductAPI to receive JWOT Bearer token

--- Mango.Services.ProductAPI/Startup.cs
+++ Mango.Services.ProductAPI/Startup.cs
@@ -40,9 +40,56 @@ namespace Mango.Services.ProductAPI
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
+
+            //add token
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options=> 
+                {
+                    //Idenity Server URL
+                    options.Authority = "https://localhost:44393/";
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+            //add authorization 
+            services.AddAuthorization(options=> {
+                //below to add policy and scope
+                //options.AddPolicy("ApiScope", policy =>
+                //{
+                //    policy.RequireAuthenticatedUser();
+                //    policy.RequireClaim("scope", "mangoServer");
+                //});
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mango.Services.ProductAPI", Version = "v1" });
+                c.EnableAnnotations();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme 
+                {
+                    Description = @"Enter 'Bearer' [space] and your token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });    
             });
         }
 
