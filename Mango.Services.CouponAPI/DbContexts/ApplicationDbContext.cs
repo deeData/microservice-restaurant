6c4545aa2e1891e1b6f0971eb5a8f54f119c7597
deedata <deidra108@gmail.com>
2021-09-10 14:56:28 -0600

    migrate seeded coupon data, implement repository coupon method

--- Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -14,6 +14,29 @@ namespace Mango.Services.CouponAPI.DbContexts
         //create table in db via code migration
         public DbSet<Coupon> Coupons { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon
+                {
+                    CouponId = 1,
+                    CouponCode = "10OFF",
+                    DiscountAmount = 10
+                });
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon
+                {
+                    CouponId = 2,
+                    CouponCode = "20OFF",
+                    DiscountAmount = 20
+                });
+        }
+
+
+
     }
 }
 
---
+++ Mango.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+
+        //CRUD operations are skipped for coupons, will have seed data records for coupons
+    }
+
+}
