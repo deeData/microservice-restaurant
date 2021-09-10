f6676a987aa5a9ad25944beb3596a90bf17dc098
deedata <deidra108@gmail.com>
2021-09-10 15:08:48 -0600

    implement CouponController endpoint for get coupon

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : Controller
+    {
+        protected ResponseDto _response;
+        private ICouponRepository _couponRepository;
+        
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            this._response = new ResponseDto();
+        }
+
+     
+        [HttpGet("{couponCode}")]
+        public async Task<object> GetDiscountForCode(string couponCode)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetCouponByCode(couponCode);
+                _response.Result = coupon;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+    }
+}
