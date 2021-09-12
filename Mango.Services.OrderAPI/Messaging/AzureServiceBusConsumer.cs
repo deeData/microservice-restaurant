f378b48deaf44949a1ec0ba0193ed752a1f8a248
deedata <deidra108@gmail.com>
2021-09-12 11:59:56 -0600

    OrderAPI to retrieve message from ServiceBus, create DTOs

---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        //message passed in from ServiceBus
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args) 
+        {
+            var message = args.Message;
+
+
+        }
+    }
+}
